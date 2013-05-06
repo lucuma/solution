f670dc5263ee3ced1b19d583aa2abd9d63be2b7c
Juan-Pablo Scaletti <juanpablo@lucumalabs.com>
2013-05-05 23:54:40 -0500

    Moves the FormSet class to a separated file

--- solution/__init__.py
+++ solution/__init__.py
@@ -9,7 +9,8 @@
     © [Lúcuma labs] (http://lucumalabs.com).
 
 """
-from .form import Form, FormSet
+from .form import Form
+from .formset import FormSet
 from .fields import *
 from .validators import *
 from .utils import Markup, get_html_attrs, to_unicode
